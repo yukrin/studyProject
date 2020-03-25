a51f550f59761f75f8b7f0f54ffdcc02ba690746
yukrin <598044716@qq.com>
2020-03-25 20:42:57 +0800

    提交

---
+++ springboot/src/test/java/cx/study/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package cx.study.springboot;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
